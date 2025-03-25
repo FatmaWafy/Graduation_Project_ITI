026daa3fe45c1f93d806e2a2afa2a958f56f779e
FatmaWafy <fatmaa1601@gmail.com>
2025-03-25 02:01:59 +0200

    just_test

--- backend/st_notifications/admin.py
+++ backend/st_notifications/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Student, Note, PredefinedNotification
+from .models import Note, PredefinedNotification
 
-admin.site.register(Student)
 admin.site.register(Note)
 admin.site.register(PredefinedNotification)
