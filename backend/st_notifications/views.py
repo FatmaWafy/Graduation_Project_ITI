608eece136102d19e5f2a9081d98c4457d29abc5
FatmaWafy <fatmaa1601@gmail.com>
2025-03-26 07:49:39 +0200

    notes_time

--- backend/st_notifications/views.py
+++ backend/st_notifications/views.py
@@ -10,10 +10,6 @@ from rest_framework import generics, serializers  # DRF classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import ListAPIView
 
-# SendNotificationView
-
-# from rest_framework.permissions import AllowAny
-
 class SendNotificationView(APIView):
     permission_classes = [AllowAny]  # Disable authentication
 
@@ -44,7 +40,6 @@ class SendNotificationView(APIView):
             status=status.HTTP_201_CREATED
         )
 
-
 class StudentNotificationListView(ListAPIView):
     """List all notifications for the logged-in student."""
     serializer_class = NotificationSerializer
@@ -54,32 +49,6 @@ class StudentNotificationListView(ListAPIView):
         """Filter notifications for the logged-in student."""
         return Note.objects.filter(receiver__user=self.request.user).order_by("-timestamp")
 
-    
-    
-# class NotificationListCreateView(generics.ListCreateAPIView):
-#     queryset = Note.objects.all()
-#     serializer_class = NotificationSerializer
-
-# # class StudentListCreateView(generics.ListCreateAPIView):
-# #     queryset = Student.objects.all()
-# #     serializer_class = StudentSerializer
-
 class PredefinedNotificationListCreateView(generics.ListCreateAPIView):
     queryset = PredefinedNotification.objects.all()
     serializer_class = PredefinedNotificationSerializer
-
-# class SendNoteView(generics.CreateAPIView):
-#     queryset = Note.objects.all()
-#     serializer_class = NotificationSerializer
-
-#     def create(self, request, *args, **kwargs):
-#         instructor_id = request.data.get("instructor_id")
-#         student_id = request.data.get("student_id")
-
-#         if not Instructor.objects.filter(id=instructor_id).exists():
-#             return Response({"error": f"Instructor with id {instructor_id} not found!"}, status=status.HTTP_400_BAD_REQUEST)
-        
-#         if not Student.objects.filter(id=student_id).exists():
-#             return Response({"error": f"Student with id {student_id} not found!"}, status=status.HTTP_400_BAD_REQUEST)
-
-#         return super().create(request, *args, **kwargs)
