eeb1f8c6965c77a8994e2366f23bfa57f53dfdf8
saraahamrr <saraahamrr98@gmail.com>
2025-04-01 21:37:58 +0200

    m

--- frontend/src/lib/date-utils.ts
+++
@@ -1,4 +0,0 @@
-// This file is just a placeholder to indicate that we need to install date-fns
-// In a real project, you would run: npm install date-fns
-// The actual import is in the notifications-dropdown.tsx file
-
