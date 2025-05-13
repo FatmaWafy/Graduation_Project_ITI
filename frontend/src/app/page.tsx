c6ed596f997e89522ef5fe70a67a6ed930969327
FatmaWafy <fatmaa1601@gmail.com>
2025-05-14 00:28:10 +0300

    add signup btn to mpblie menu

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -83,11 +83,11 @@ export default function LandingPage() {
               Login
             </Button>
           </Link>
-          {/* <Link href='/signup' onClick={toggleMenu}>
+          <Link href='/signup' onClick={toggleMenu}>
             <Button className='bg-transparent border text-white hover:border-transparent hover:text-white/80 font-medium rounded-xl px-4 w-32 text-center'>
               Signup
             </Button>
-          </Link> */}
+          </Link>
           <Link href='/faq' onClick={toggleMenu}>
             <Button className='bg-transparent border text-white hover:border-transparent hover:text-white/80 font-medium rounded-xl px-4 w-32 text-center'>
               FAQ
