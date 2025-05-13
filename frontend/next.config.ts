ae310b334ce5c891c776562d09c26e9a634c19fd
FatmaWafy <fatmaa1601@gmail.com>
2025-05-13 17:57:47 +0300

    deploy

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -14,10 +14,20 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: "/api/:path*",
-        destination: `${origin}/api/:path*`,
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
       },
     ];
+  },
+  webpack: (config, { isServer }) => {
+    // إعدادات Webpack لتجاهل 'fs' في الـ client-side
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      };
+    }
+    return config;
   },
 };
 
